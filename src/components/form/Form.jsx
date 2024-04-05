9a139ede0f84c0c218d5d739c8b6b6eb217a91fb
Marius Du Plooy <woestyn101@gmail.com>
2024-04-05 15:34:55 -0400

    form validation

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,27 +1,98 @@
 import { useState } from 'react';
 import '../../styles/index.css';
+import { validateEmail } from '../../utils/helpers';
 
 function Form() {
-  // Here we set two state variables for firstName and lastName using `useState`
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (e)=> {
+      if (e.target.value.length > 0)
+      {
+       setErrorMessage('');
+     } 
+      else if (e.target.name === "name" && !e.target.value.length)
+       {
+        setErrorMessage('Name is required');
+      } 
+      else if (e.target.name === "email" && !e.target.value.length)
+      {
+        setErrorMessage('email is required');
+      }
+      else if (e.target.name === "message" && !e.target.value.length)
+      {
+        setErrorMessage('Message is required');
+      }
+      else {
+        setErrorMessage('');
+      }
+    }
   
+  const handleSubmit = (e) =>{
+    const userInput = {name, email, message};
+
+    e.preventDefault();
+    if (!validateEmail(userInput.email)) {
+       setErrorMessage('Email is invalid');
+       return;
+     
+        }
+  
+
+   
+    console.log(userInput.name);
+    console.log(userInput.email);
+    console.log(userInput.message);
+    alert("Thank you " + userInput.name + " for your message!");
+    
+  }
 
   return (
     <div className="create">
         <div className="form-content">
         <h2>Leave your name, email and message:</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name:</label>
             <input
-            type="text"/>
+            type="text"
+            name="name"
+            value={name}
+            onBlur={handleChange}       
+            
+           onChange={(e)=> setName(e.target.value)}
+            
+            />
              <label>Email:</label>
             <input
-            type="text"/>
+            type="text"
+            value={email}
+            name="email"
+            onBlur={handleChange}      
+            onChange={(e)=> setEmail(e.target.value)}/>
              <label>Message:</label>
-            <textarea></textarea>
+            <textarea   value={message}
+            name="message"
+             onBlur={handleChange} 
+             onChange={(e)=> setMessage(e.target.value)}
+             ></textarea>
             <button>Submit</button>
-
         </form>
+        {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
         </div>
+      )}
+        <p>{name}</p>
+        <p>{email}</p>
+        <p>{message}</p>
+        </div>
+        
+        
+
+       
+       
 
 
 
